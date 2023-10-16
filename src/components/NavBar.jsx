7d58d14085c693a14fd934b3cae631aa5247a849
Pat <hoang.patrick@outlook.com>
2023-10-16 14:49:41 +0200

    ajout NavBar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import "./NavBar.css"
+import logo from '../assets/logo.png'
+import 
+export default function NavBar(){
+
+    return (
+        <nav>
+            <img src= {logo} alt="" />
+            <p id="titre">мечка</p>
+            <p>log in</p>
+        </nav>
+    )
+}
