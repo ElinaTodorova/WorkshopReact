57fcddff5462ba557f5c622f5363e545adb1ddf2
Nas-lmr <nasreddine-lamamra_student2023@wilder.school>
2023-10-16 16:23:44 +0200

    add compteur component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import CompteurLike from "./assets/components/compteur_likes/compteur";
 
 function App() {
   return (
-    <>
-      
-    </>
-  )
+    <div>
+      <CompteurLike />
+    </div>
+  );
 }
 
-export default App
+export default App;
