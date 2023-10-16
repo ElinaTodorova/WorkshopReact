fea7b521a0d01f6181a777777ecb87b4871e3858
Nas-lmr <nasreddine-lamamra_student2023@wilder.school>
2023-10-16 17:11:40 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
-import NavBar from './components/NavBar'
-import { useState } from 'react'
-import './App.css'
-import CartMemes from './components/CardMemes/CardMemes.jsx'
-import CardContainer from './components/CardContainer/CardContainer.jsx'
-
+import NavBar from "./components/NavBar";
+import { useState } from "react";
+import "./App.css";
+import CartMemes from "./components/CardMemes/CardMemes.jsx";
+import CardContainer from "./components/CardContainer/CardContainer.jsx";
 
 function App() {
- 
   return (
-
-
-  <NavBar/>
-  <CardContainer />
-
-)}
-
+    <div>
+      <NavBar />
+      <CardContainer />
+    </div>
+  );
+}
 
 export default App;
