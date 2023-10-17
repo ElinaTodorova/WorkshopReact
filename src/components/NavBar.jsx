892f1f71342d5ee3bda177d5fcff5c5d516adb3d
Pat <hoang.patrick@outlook.com>
2023-10-17 10:14:23 +0200

    alerte added on login button

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,19 @@
 import "./NavBar.css"
 import logo from '../assets/logo.png'
+import maintenance from '../assets/maintenance-icon.svg'
+
+
 
 export default function NavBar(){
 
+ function handleClickLogin () {
+ alert (<img src={maintenance}/> + "Under maintenance, please comme back later.")
+ }
     return (
         <nav>
             <img src= {logo} alt="logo мечка" />
             <p id="titre">мечка</p>
-            <button>Log In</button>
+            <button onClick={handleClickLogin}>Log In</button>
         </nav>
     )
 }
